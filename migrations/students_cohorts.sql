a05e5ec126d649db7679dc51e76ec882dc5d41f3
mascott82 <m.a.scott@live.com>
2023-12-10 22:14:45 -0600

    Initial commit.

---
+++ migrations/students_cohorts.sql
@@ -0,0 +1,20 @@
+
+-- Create a table to store information about cohorts in the Bootcamp
+CREATE TABLE cohorts (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  start_date DATE,
+  end_date DATE
+);
+
+-- Create a table to store information about students in the Bootcamp
+CREATE TABLE students (
+  id  SERIAL PRIMARY KEY NOT NULL,
+  name  VARCHAR(255) NOT NULL,
+  email VARCHAR(255),
+  phone VARCHAR(32),
+  github  VARCHAR(255), 
+  start_date  DATE,
+  end_date  DATE,
+  cohort_id INTEGER REFERENCES cohorts(id) ON DELETE CASCADE
+);
